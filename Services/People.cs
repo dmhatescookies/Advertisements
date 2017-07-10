4dab1702c42823bd0bbff1473ed16f74a8b99e8e
Марищук Дмитро <pidmogylnyi@gmail.com>
2017-07-10 19:34:29 +0300

    Update Services

---
+++ Services/People.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdvertisementsServer;
+
+namespace Ad
+{
+    class People
+    {
+        public List<Person> PersonList;
+        private DbDatabase db;
+
+        public People ()
+        {
+            db = new DbDatabase();
+            PersonList = db.GetAllPersons();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return PersonList.Count;
+            }
+        }
+
+        public void Add(Person item)
+        {
+            if (!db.PersonExist(item))
+                db.AddPerson(item);
+            PersonList = db.GetAllPersons();
+        }
+
+
+        public List<Person> Sort(List<Person> list, int byWhat)
+        {
+            List<Person> sortedList = null;
+            switch (byWhat)
+            {
+                case 1: //Firstname
+                    sortedList = list.OrderBy(a => a.PersonFirstname).ToList();
+                    break;
+
+                case 2: //Lastname
+                    sortedList = list.OrderBy(a => a.PersonLastname).ToList();
+                    break;
+
+                case 3: //RegistrationTime
+                    sortedList = list.OrderBy(a => a.RegistrationTime).ToList();
+                    break;
+
+                default:
+                    break;
+
+            }
+            return sortedList;
+        }
+
+        public List<Person> Search(string keyword)
+        {
+            List<Person> resultList = new List<Person>();
+            PersonList = db.GetAllPersons();
+            foreach (Person person in PersonList)
+                if (person.PersonFirstname == keyword || person.PersonLastname == keyword || person.PhoneNumber == keyword ||
+                    person.Email == keyword)
+                    resultList.Add(person);
+            return resultList;
+        }
+
+
+        public override string ToString()
+        {
+            string result = "";
+            foreach (Person person in PersonList)
+            {
+                result += person.ToString();
+            }
+            return result;
+        }
+    }
+}
---
+++ Services/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Ad
+{
+    public class Person
+    {
+        [XmlIgnore]
+        [Key]
+        public int PersonId { get; set; }
+        public string PersonFirstname { get; set; }
+        public string PersonLastname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string RegistrationTime { get; set; }
+        public string Password { get; set; }
+
+        public Person() { }
+
+        public Person(string firstName, string lastName, string phoneNumber, string email, string pass)
+        {
+            this.RegistrationTime = DateTime.Now.ToString();
+            this.PersonFirstname = firstName;
+            this.PersonLastname = lastName;
+            this.PhoneNumber = phoneNumber;
+            this.Email = email;
+            this.Password = pass;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Person forCompare = (Person)obj;
+            return this.Email.Equals(forCompare.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("ID:{0}, Firstname: {1},  Lastname: {2}, Phone number: " +
+                "{3}, Email: {4}, Data of Registration: {5}",
+                PersonId, PersonFirstname, PersonLastname, PhoneNumber, Email, RegistrationTime.ToString());
+        }
+
+    }
+}
